4547da6fcbc2d349cfc3ff25107075bcfaed2586
baaaabbaaaba <3tgsegesgzseg@gmail.com>
2022-08-16 20:16:57 +0900

    Windowに縦線表示

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -45,9 +45,12 @@ public class GamePanel extends JPanel implements ActionListener{
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		draw(g);
-	}
+	}                                                          
 	public void draw(Graphics g) {
 		
+		for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
+			g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+		}
 	}
 	public void newApple() {
 		
